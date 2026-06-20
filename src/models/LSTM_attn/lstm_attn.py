02e4129001dd601da39e741adfa2a16f03cc5bdc
007Vd <vdeogade007@gmail.com>
2026-06-20 19:04:21 +0530

    LSTM Attention model created

--- src/models/LSTM_attn/lstm_attn.py
+++ src/models/LSTM_attn/lstm_attn.py
@@ -9,10 +9,12 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 DF=Path("/Users/007vd/Downloads/DAU/Split_Graph_dl/data/simple_mlp/return_dataframe/return_df")
 
+#%%
 returns_df=pd.read_csv(DF)
 print(returns_df.columns)
 returns_df=returns_df.drop(columns=["Date",'Unnamed: 0'])
 print(returns_df)
+
 
 # %%
 returns=returns_df.values
@@ -29,10 +31,6 @@ y=np.array(y)
 
 print(X.shape)
 print(y.shape)
-
-# %%
-X=X.reshape(X.shape[0],-1)
-X.shape
 # %%
 split=int(0.8*len(X))
 X_train = X[:split]
@@ -41,12 +39,17 @@ y_train =y[:split]
 X_test = X[split:]
 y_test = y[split:]
 
-
 X_train = torch.tensor(X_train, dtype=torch.float32)
 Y_train = torch.tensor(y_train, dtype=torch.float32)
 
 X_test = torch.tensor(X_test, dtype=torch.float32)
 Y_test = torch.tensor(y_test, dtype=torch.float32)
+
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
 # %%
 class CustomDataset(Dataset):
     def __init__(self,features,labels):
@@ -65,31 +68,59 @@ test_dataset=CustomDataset(X_test,y_test)
 train_loader=DataLoader(train_dataset,batch_size=len(train_dataset),shuffle=False,pin_memory=True)
 test_loader=DataLoader(test_dataset,batch_size=len(test_dataset),shuffle=False,pin_memory=True)
 # %%
-class PortfolioMLP(nn.Module):
-    def __init__(self,num_features):
+class HistoricalState(nn.Module):
+    def __init__(self,hidden_dim,input_dim):
         super().__init__()
-        self.net=nn.Sequential(
-            nn.Linear(num_features,256),
-            # nn.BatchNorm1d(256),
-            nn.ReLU(),
-            # nn.Dropout(p=0.3),
-            nn.Linear(256,128),
-            # nn.BatchNorm1d(128),
-            nn.ReLU(),
-            # nn.Dropout(p=0.3),
-            nn.Linear(128,15)
+        self.W1=nn.Linear(2*hidden_dim,hidden_dim,bias=False)
+        self.W2=nn.Linear(input_dim,hidden_dim,bias=False)
+        self.Ve=nn.Parameter(torch.randn(hidden_dim))
+
+    def forward(self,H,X_last):
+        B,L,Hdim=H.shape
+        H_last=H[:,-1,:]
+        H_last=H_last.unsqueeze(1)
+        H_last=H_last.repeat(1,L,1)
+        concat=torch.cat([H,H_last],dim=-1)
+        term1=self.W1(concat)
+        term2=self.W2(X_last).unsqueeze(1)
+        e=torch.tanh(term1+term2)
+
+        scores=torch.matmul(e,self.Ve)
+
+        alpha=torch.softmax(scores,dim=1)
+        E=(alpha.unsqueeze(-1)*H).sum(dim=1)
+
+        return E,alpha
+    
+class PortfolioLSTM(nn.Module):
+    def __init__(self,num_assets=15,hidden_dim=256):
+        super().__init__()
+        self.lstm=nn.LSTM(
+            input_size=num_assets,
+            hidden_size=hidden_dim,
+            batch_first=True
         )
 
+        self.attn=HistoricalState(hidden_dim,num_assets)
+        self.mlp=nn.Sequential(
+            nn.Linear(hidden_dim,256),
+            nn.ReLU(),
+            nn.Linear(256,128),
+            nn.ReLU(),
+            nn.Linear(128,num_assets)
+            )
+    
     def forward(self,x):
-        logits=self.net(x)
+        H,_=self.lstm(x)
+        X_last=x[:,-1,:]
+        E,alpha=self.attn(H,X_last)
+        logits=self.mlp(E)
         weights=F.softmax(logits,dim=1)
-
         return weights
-
 # %%
-model=PortfolioMLP(X_train.shape[1])
+model=PortfolioLSTM(X_train.shape[2])
 print(sum(p.numel() for p in model.parameters()))
-model
+print(model)
 # %%
 def sharpe_loss(weights,future_returns):
     portfolio_returns=(weights*future_returns).sum(dim=1)
@@ -102,9 +133,7 @@ def sharpe_loss(weights,future_returns):
     return -annualized_sharpe
 
 optimizer=torch.optim.Adam(model.parameters(),lr=0.001,weight_decay=1e-4)
-
-
-#%%
+# %%
 def annualized_volatility(returns):
     return returns.std() * np.sqrt(252)
 
@@ -129,11 +158,11 @@ def cumulative_return(returns):
     wealth = torch.cumprod(1 + returns,dim=0)
     return wealth[-1] - 1
 
-#%%
+# %%
 train_sharpes = []
 val_sharpes = []
 
-num_epochs = 35
+num_epochs = 24
 
 for epoch in range(num_epochs):
     model.train()
@@ -201,25 +230,13 @@ for epoch in range(num_epochs):
         f"Val Sharpe: {val_sharpe:.4f}"
         
     )
-    print(f"Simple MLP Annualized volatility: {annualized_volatility(val_returns)}")
-    print(f"Simple MLP Sortino Ratio: {sortino_ratio(val_returns)}")
-    print(f"Simple MLP Max DrawDown: {max_drawdown(val_returns)}")
-    print(f"Simple MLP cumulative returns: {cumulative_return(val_returns)}")
+    print(f"Attention LSTM Annualized volatility: {annualized_volatility(val_returns)}")
+    print(f"Attention LSTM Sortino Ratio: {sortino_ratio(val_returns)}")
+    print(f"Attention LSTM Max DrawDown: {max_drawdown(val_returns)}")
+    print(f"Attention LSTM cumulative returns: {cumulative_return(val_returns)}")
 
 
-
-#%%
-model.eval()
-
-with torch.no_grad():
-    weights = model(X_test)
-
-avg_weights = weights.mean(dim=0)
-
-print(avg_weights)
-print(avg_weights.sum())
-print(weights.std(dim=0))
-
+# %%
 print("equal weights")
 
 equal_weights = torch.ones(15) / 15
@@ -234,7 +251,6 @@ print(f"Equal Weights cumulative returns: {cumulative_return(eq_returns)}")
 
 print("equal weight sharpe is :",eq_sharpe)
 
-#%%
 import matplotlib.pyplot as plt
 
 train_losses = [-x for x in train_sharpes]
@@ -252,7 +268,7 @@ ax[0].plot(val_losses,label="Validation Loss")
 
 ax[0].set_title("Loss vs Epoch")
 ax[0].set_xlabel("Epoch")
-ax[0].set_ylabel("Loss")
+ax[0].set_ylabel("Los`s")
 
 ax[0].legend()
 ax[0].grid(True)
@@ -272,43 +288,4 @@ plt.tight_layout()
 plt.show()
 
 
-# %%
-# 
-
-#%%
-# import optuna
-
-# study = optuna.create_study(
-#     direction="maximize"
-# )
-
-# study.optimize(
-#     objective,
-#     n_trials=50
-# )
-
-# print(
-#     "Best Sharpe:",
-#     study.best_value
-# )
-
-# print(
-#     study.best_params
-# )
-
-
-# %%
-# equal_weights = torch.ones(15) / 15
-
-# portfolio_returns = (
-#     Y_test * equal_weights
-# ).sum(dim=1)
-
-# equal_weight_sharpe = (
-#     portfolio_returns.mean()
-#     /
-#     (portfolio_returns.std() + 1e-8)
-# )
-
-# print(equal_weight_sharpe*np.sqrt(252))
 # %%
